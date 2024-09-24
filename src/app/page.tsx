eac1bfaa3e9084f80bef8305add3093778b711dc
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-09-24 23:51:10 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 export default function Home() {
   return(
     <>
-    <h1 className="">Hey there !👋 <br />
-    This site is under construction.</h1>
+    <h1 className="">Hey there ! 👋 <br />
+    This site is under construction. <br />
+     by <a href="https://github.com/uzairrehan">Uzair Rehan 🔗</a>
+    </h1>
     </>
   )
 }
